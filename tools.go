af72ccbaa38ce748cd456780f1e13522ca5aa9cb
Alex Girenko <aspnet123@gmail.com>
2023-03-06 22:14:20 +0100

    modification

--- tools.go
+++ tools.go
@@ -6,6 +6,7 @@ const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 
 // Tools is the type used to instantiate this module. Any variable of this type will have access
 // to all the methods with the receiver *Tools
+// small change to git
 type Tools struct{}
 
 // RandomString returns a string of random characters of length n, using randomStringSource
